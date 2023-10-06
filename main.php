7ff84c14c860904a712575e076f7525a743a077d
Hirotada <68522105+hirotada-t@users.noreply.github.com>
2023-10-06 19:53:29 +0900

    main php file

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: AB Test Field
+Description: This plugin adds custom field at edit page to set AB test.
+Version: 1.0.0
+Author: sizebook
+Author URI: https://sizebook.co.jp/
+*/
+
+if (!defined('ABSPATH')) exit;
+
+require_once plugin_dir_path(__FILE__) . 'edit_page.php';
+require_once plugin_dir_path(__FILE__) . 'redirect.php';
