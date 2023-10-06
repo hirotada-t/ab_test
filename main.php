c4f73294dd66eb680b13e02a14da1dff857b9af6
Hirotada <68522105+hirotada-t@users.noreply.github.com>
2023-10-06 18:27:27 +0900

    calc probability

--- main.php
+++ main.php
@@ -18,17 +18,21 @@ add_action('add_meta_boxes', 'add_custom_fields_meta_box');
 // メタボックスのコールバック関数
 function custom_fields_callback($post)
 {
-  wp_nonce_field('save_custom_fields_data', 'custom_fields_meta_box_nonce');
+  wp_nonce_field('save_ab_test_fields_data', 'custom_fields_meta_box_nonce');
+
+  $denominator = calc_total_value($post->ID);
 
   for ($i = 1; $i <= 5; $i++) {
     $path_or_url = get_post_meta($post->ID, "path_or_url_$i", true);
-    $probability = get_post_meta($post->ID, "probability_$i", true);
+    $ratio = get_post_meta($post->ID, "probability_$i", true);
+    $probability = $denominator > 0 ? round(intval($ratio) / $denominator * 100) : 0;
 
     $field = <<<EOM
-    <label for="path_or_url_$i">Path or URL $i: </label>
-    <input type="text" id="path_or_url_$i" name="path_or_url_$i" value="$path_or_url" required>
-    <label for="probability_$i" style="margin-left:20px;">Probability $i: </label>
-    <input type="number" id="probability_$i" name="probability_$i" value="$probability" step="1" min="0" max="10"><br><br>
+    <div style="margin:20px 0;">
+      <label for="path_or_url_$i">Path or URL $i: </label>
+      <input type="text" id="path_or_url_$i" name="path_or_url_$i" value="$path_or_url">/
+      <input type="number" id="probability_$i" name="probability_$i" value="$ratio" step="1" min="0" max="10">($probability%)
+    </div>
     EOM;
 
     echo $field;
@@ -36,10 +40,10 @@ function custom_fields_callback($post)
 }
 
 // カスタムフィールドのデータ保存
-function save_custom_fields_data($post_id)
+function save_ab_test_fields_data($post_id)
 {
   if (!isset($_POST['custom_fields_meta_box_nonce'])) return;
-  if (!wp_verify_nonce($_POST['custom_fields_meta_box_nonce'], 'save_custom_fields_data')) return;
+  if (!wp_verify_nonce($_POST['custom_fields_meta_box_nonce'], 'save_ab_test_fields_data')) return;
 
   for ($i = 1; $i <= 5; $i++) {
     if (array_key_exists("path_or_url_$i", $_POST)) {
@@ -50,4 +54,14 @@ function save_custom_fields_data($post_id)
     }
   }
 }
-add_action('save_post', 'save_custom_fields_data');
+add_action('save_post', 'save_ab_test_fields_data');
+
+function calc_total_value($post_id)
+{
+  $total_value = 0;
+  for ($i = 1; $i <= 5; $i++) {
+    $probability_value = get_post_meta($post_id, "probability_$i", true);
+    $total_value += intval($probability_value);
+  }
+  return $total_value;
+}
