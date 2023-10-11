41cedd8afbe869de45439c522afd44b27a771c2f
Hirotada <68522105+hirotada-t@users.noreply.github.com>
2023-10-11 20:27:13 +0900

    feat: keep url query

--- redirect.php
+++ redirect.php
@@ -4,38 +4,40 @@ function ab_test_redirect()
 {
   $post_id = get_the_ID();
   $run_test = get_post_meta($post_id, "run_test", true);
-
+  
   if (is_single() && $run_test) {
+    $fields_list = get_post_meta($post_id, "fields_list", true);
+    $fields_list = array_filter($fields_list, function ($item) {
+      return !empty($item['path']) || !empty($item['ratio']);
+    });
+    $fields_list = array_values($fields_list);
     $root_path = get_page_uri($post_id);
     $error = get_post_meta($post_id, "error", true);
     if ($error > 0) {
       return;
     }
-
-    $redirects = [];
+    
     $total_ratio = 0;
-
-    for ($i = 1; $i <= 5; $i++) {
-      $path = get_post_meta($post_id, "path_$i", true);
-      $ratio = get_post_meta($post_id, "ratio_$i", true);
-
-      if (!empty($path) && !empty($ratio)) {
-        $redirects[] = [
-          'path' => $path,
-          'ratio' => $ratio
-        ];
+    for ($i = 0; $i < count($fields_list); $i++) {
+      $ratio = $fields_list[$i]['ratio'];
+      if (!empty($ratio)) {
         $total_ratio += $ratio;
       }
     }
-
-    $rand_num = mt_rand(0, $total_ratio);
+    
+    $rand_num = mt_rand(1, $total_ratio);
     $current_ratio = 0;
-
-    foreach ($redirects as $redirect) {
-      $current_ratio += $redirect['ratio'];
+    $current_query_string = $_SERVER['QUERY_STRING'];
+    if (!empty($current_query_string)) {
+      $current_query_string = '?' . $current_query_string;
+    }
+    foreach ($fields_list as $fields) {
+      $current_ratio += intval($fields['ratio']);
       if ($rand_num <= $current_ratio) {
-        if ($redirect['path'] !== $root_path) {
-          wp_redirect(home_url($redirect['path']));
+        if ($fields['path'] === $root_path) {
+          return;
+        } else {
+          wp_redirect(home_url($fields['path'] . $current_query_string));
           exit;
         }
       }
